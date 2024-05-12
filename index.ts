14dc786dcddcbdac3fe065c9a0682a702ef35972
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-11 20:11:07 -0400

    feat: Add Swapper functionality and dependencies
    This commit adds the necessary code changes to implement the Swapper functionality in the index.ts file. It imports the 'intro' and 'outro' functions from the '@clack/prompts' package to display welcome and farewell messages to the user.
    
    Additionally, the package.json file is updated to include the '@clack/core' and '@clack/prompts' dependencies.
    
    These changes enable Swapper to provide a user-friendly experience and enhance its functionality.

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { intro, outro } from '@clack/prompts';
+
+intro("Welcome to Swapper -- the slack tool to serve your every prankster need!");
+
+// sign user in
+
+// confirm user is signed in and ready to swap
+
+// if profile is saved, ask if user wants to restore it
+
+// else
+// choose user to swap with
+
+// confirm swap
+
+// swap
+
+// confirm swap completed
+
+// notify user that their old profile is saved
+
+outro("Thanks for using Swapper! Have a great day!");
